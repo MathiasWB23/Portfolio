4a70fe255eaf00bfefc5280781754782d877fab6
Mathias Willum Bødker <mathiaswb1600@gmail.com>
2025-01-17 14:43:38 +0100

    Add files via upload

---
+++ C# - Programming/Hello World1/Hello World1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello, world!");
+            //Når vi anvender Console, åbnes et Console vindue hvori der skal skrives en line tekst med WriteLine. Herefter skal vi have defineret hvad tekst der skal stå deri. Og så skal komandoen stoppes og spring ned til næste linje
+            Console.ReadLine();
+            //Readline, computeren tjekker om linjen under er null eller tom, så lukkes vinduet.
+            
+        }
+    }
+}
